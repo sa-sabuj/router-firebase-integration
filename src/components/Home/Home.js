13ce7fb53118f572cc67f53ab146bb767281806b
sa-sabuj <sasabuj321@gmail.com>
2022-05-12 04:25:19 +0600

    react firebase hook used

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Home = () => {
+    const { user } = useFirebase();
     return (
         <div>
             <h2>This is home</h2>
+            <p>Current user : {user ? user.displayName : "User is not Loged in"}</p>
         </div>
     );
 };
